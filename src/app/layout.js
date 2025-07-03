5c3ffe351a1bfdf77ebd499cbcb1344417f772e0
Solomon Olufelo <solomonolufelo@outlook.com>
2025-07-03 14:22:14 -0400

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import '../styles/globals.css'
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="bg-gray-900 text-white">
         {children}
       </body>
     </html>
